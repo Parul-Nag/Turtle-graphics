2aad18ce0b6bb92d2f330db461eb47486a3afbab
Parul-Nag <nagparul1@gmail.com>
2024-08-10 18:16:51 +0530

    storing 2 turtle graphics design
    storing 2 turtle graphics design from local to remote.

---
+++ star.py
@@ -0,0 +1,15 @@
+import turtle
+import time
+t=turtle.Turtle()
+t.speed(10)
+t.forward(250)
+t.right(145)
+t.forward(250)
+t.right(145)
+t.forward(250)
+t.right(138)
+t.forward(248)
+t.right(144)
+t.forward(267)
+
+time.sleep(15)
