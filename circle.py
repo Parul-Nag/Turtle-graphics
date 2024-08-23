c1ffd35b5c079c82faeee5fdc1f55f450033077a
Parul-Nag <nagparul1@gmail.com>
2024-08-23 12:27:18 +0530

    new files are uploaded

---
+++ circle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 13:27:19 2024
+
+@author: ITM-USER
+"""
+
+import turtle
+t=turtle.Turtle()
+t.circle(100,180)
+t.left(90)
+t.forward(200)
+t.right(110)
+t.forward(300)
---
+++ square_circle.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 12:08:04 2024
+
+@author: ITM-USER
+"""
+
+import turtle
+t=turtle.Turtle()
+
+for i in range(1000):
+    t.speed(0.5)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward((100))
+    t.left(90)
+    t.left(60.5)
