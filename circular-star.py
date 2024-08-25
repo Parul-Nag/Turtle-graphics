39e4a3e700a9c1a0b7332e7238fdea95631d1757
Parul-Nag <nagparul1@gmail.com>
2024-08-25 14:09:08 +0530

    3 new files are uploaded from local

---
+++ circular-star.py
@@ -0,0 +1,17 @@
+import turtle
+import time
+t=turtle.Turtle()
+t.speed(0.5)
+for i in range(500):
+    t.forward(250)
+    t.right(145)
+    t.forward(250)
+    t.right(145)
+    t.forward(250)
+    t.right(138)
+    t.forward(248)
+    t.right(144)
+    t.forward(267)
+    t.right(123.5)
+
+#time.sleep(15)
